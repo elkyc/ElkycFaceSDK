a4baeb37f4227e1239065107f6b84b3194a39a22
Alex Gurin <dn070287gav@gmail.com>
2021-05-25 18:37:46 +0300

    version 1.0.1

---
+++ ElkycFaceSDK.xcframework/ios-arm64/ElkycFaceSDK.framework/Headers/FaceSDK.h
@@ -0,0 +1,18 @@
+//
+//  FaceSDK.h
+//  FaceSDK
+//
+//  Created by Alex Gurin on 3/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaceSDK.
+FOUNDATION_EXPORT double FaceSDKVersionNumber;
+
+//! Project version string for FaceSDK.
+FOUNDATION_EXPORT const unsigned char FaceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceSDK/PublicHeader.h>
+
+
