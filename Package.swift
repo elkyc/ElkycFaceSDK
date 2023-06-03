480cc75daadeadf34d8c8a9a22013606990433eb
Sergey Chausov <chausov@users.noreply.github.com>
2023-06-03 22:13:25 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "ElkycFaceSDK",
-            targets: ["ElkycFaceSDK", "FaceSDK", "Liveness"]),
+            targets: ["ElkycFaceSDK", "FaceSDK", "Liveness", "RegulaCommon"]),
     ],
     targets: [
         .binaryTarget(
@@ -28,6 +28,5 @@ let package = Package(
             name: "RegulaCommon",
             url: "https://pods.regulaforensics.com/RegulaCommon/6.8.324/RegulaCommon-6.8.324.zip",
             checksum: "8e82f1e4cfd538261486091362bd26b763472f77ee3ef160d22639c55beeed6e")
-        
     ]
 )
