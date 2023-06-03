6e86599dda270b8c31796ac9efd340a6d23de6cd
Sergey Chausov <chausov@users.noreply.github.com>
2023-06-03 22:11:14 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,6 +23,11 @@ let package = Package(
         .binaryTarget(
             name: "Liveness",
             url: "https://pods.regulaforensics.com/FaceCoreSDK/5.1.315/FaceCoreSDK-5.1.315.zip",
-            checksum: "97d27f30798fecd8dc4ece4a3f85a393f8c956f4beccbe510fcdb3a4d8103f2a")
+            checksum: "97d27f30798fecd8dc4ece4a3f85a393f8c956f4beccbe510fcdb3a4d8103f2a"),
+        .binaryTarget(
+            name: "RegulaCommon",
+            url: "https://pods.regulaforensics.com/RegulaCommon/6.8.324/RegulaCommon-6.8.324.zip",
+            checksum: "8e82f1e4cfd538261486091362bd26b763472f77ee3ef160d22639c55beeed6e")
+        
     ]
 )
