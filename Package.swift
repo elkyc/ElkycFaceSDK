fbacde5591570dd751d31074d7f42727bea95b07
Sergey Chausov <chausov@users.noreply.github.com>
2023-06-03 22:02:36 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "ElkycFaceSDK",
-            targets: ["ElkycFaceSDK", "FaceSDK", "FaceCoreSDK"]),
+            targets: ["ElkycFaceSDK", "FaceSDK", "Liveness"]),
     ],
     targets: [
         .binaryTarget(
@@ -21,7 +21,7 @@ let package = Package(
             url: "https://pods.regulaforensics.com/FaceSDK/5.1.1472/FaceSDK-5.1.1472.zip",
             checksum: "d47a1d1ffb7f87ed2a22fcecc675f398d688d7a1b80d673009ca2e443a82d159"),
         .binaryTarget(
-            name: "FaceCoreSDK",
+            name: "Liveness",
             url: "https://pods.regulaforensics.com/FaceCoreSDK/5.1.315/FaceCoreSDK-5.1.315.zip",
             checksum: "97d27f30798fecd8dc4ece4a3f85a393f8c956f4beccbe510fcdb3a4d8103f2a")
     ]
